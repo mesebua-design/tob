2094f924d4d01a236f861807bb2f2267f897f590
mesebua-design <m_esebua@cu.edu.ge>
2026-08-18 08:51:40 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
+
 options.add_argument("--headless=new")
-options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1920,1080")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 10)
